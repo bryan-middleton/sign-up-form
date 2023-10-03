d66aa3cda7a07383a52ab777b5666062eb2683ed
Bryan Middleton <bryanmiddleton1986@gmail.com>
2023-10-03 14:56:55 +0100

    clear queue

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-const createButton = document.querySelector('#create');
-const form = document.querySelector('form');
-const password = document.getElementById('#password');
-const confPassword = document.getElementById('#conf-password');
-const passwordError = document.querySelector('#password + span.error');
+const createButton = document.querySelector("#create");
+const form = document.querySelector("form");
+const password = document.getElementById("#password");
+const confPassword = document.getElementById("#conf-password");
+const passwordError = document.querySelector("#password + span.error");
 
-confPassword.addEventListener('input', (event)=>{
-    if(confPassword.textContent!== password.textContent){
-        passwordError.textContent="passwords do not match";
-    }
+confPassword.addEventListener("input", (event) => {
+  if (confPassword.textContent !== password.textContent) {
+    passwordError.textContent = "passwords do not match";
+  }
 });
